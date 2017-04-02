aec44c920ae249a21183fd6bb9f70ac0d6f32c2a
Lplenka <laluprasad.lenka95@gmail.com>
2017-04-02 19:53:43 +0530

    Electron initiated :rocket:

---
+++ app/main.js
@@ -0,0 +1,11 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      height: 600,
+      width: 800
+  });
+
+});
