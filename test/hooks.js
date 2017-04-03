3c41b51866df44cc8554359d890f06f75e73909d
Lplenka <laluprasad.lenka95@gmail.com>
2017-04-03 15:47:58 +0530

    Added tests

---
+++ test/hooks.js
@@ -0,0 +1,27 @@
+var Application = require('spectron').Application;
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var electron = require('electron-prebuilt');
+
+global.before(function () {
+    chai.should();
+    chai.use(chaiAsPromised);
+});
+
+beforeEach(function () {
+    this.app = new Application({
+        path: electron,
+        args: ['app']
+    });
+
+    return this.app.start().then(function (app) {
+        chaiAsPromised.transferPromiseness = app.transferPromiseness;
+        return app
+    })
+});
+
+afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+        return this.app.stop()
+    }
+});
